024f57f486568c92ae41b149b76187651c06c2d4
danilozte <danilozte@hotmail.com>
2021-07-09 11:06:32 -0500

    "commit 1  encender un LED y guardarlo en el repo remoto "

---
+++ EXAMEN.ino
@@ -0,0 +1,16 @@
+int led = 27;
+
+void setup(){
+Serial.begin (115200);
+pinMode(led , OUTPUT);
+
+}
+
+void loop(){
+
+digitalWrite( led , HIGH);
+delay(200);
+digitalWrite( led , LOW);
+delay(200);
+
+}
