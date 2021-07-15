99ca713c33e6fed0f06af25879bf939ef39e0cd7
danilozte <danilozte@hotmail.com>
2021-07-15 11:24:18 -0500

    ultimos cambios en el codigo de flujo de control

---
+++ CICLO FOR_WHILE/CONFIG_PINES_FOR.ino
@@ -0,0 +1,20 @@
+// configuracion de varibles
+int LED = 16;
+void setup(){
+    pinMode(LED,OUTPUT);
+    Serial.begin(9600);
+    for (int leds = 15; leds <= 18; leds++)
+    {
+        pinMode(leds,OUTPUT);/* instruccion  */
+    }
+
+     for (int leds = 15; leds <= 18; leds++)
+    {
+        digitalWrite(leds,HIGH);/* instruccion  */
+    }
+    
+}
+
+void loop(){
+digitalWrite(LED,HIGH);
+}
