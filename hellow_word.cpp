134c9d1afd8a5a9a9a0af44a88d74db960cb8ef2
danilozte <danilozte@hotmail.com>
2021-07-15 11:24:50 -0500

    jhg

---
+++ hellow_word.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include <stdlib.h>
+#define PI 3.141592
+
+int main(){
+    double r=077, longitud;
+    longitud=2*PI*r;
+    printf ("la longitud de la circuferencia de radio es: ", r , longitud);
+}
